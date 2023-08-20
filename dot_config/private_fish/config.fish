2078f2e4986d2aaade0ae5004ef6e52a7e33d370
victorvintorez <victorvintorez@proton.me>
2023-08-20 21:18:27 +0100

    Initial Commit

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -g theme_powerline_fonts no
+set -g theme_nerd_fonts yes
+set -g theme_display_hostname ssh
